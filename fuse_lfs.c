7eb9ac993067efa6b2afb9f74ba6b1ecb218a541
Theophilus Mensah <theomens@man.cs.rutgers.edu>
2013-12-06 23:00:11 -0500

    update

--- fuse_lfs.c
+++ fuse_lfs.c
@@ -10,13 +10,19 @@
 #include <sys/time.h>
 #include <dirent.h>
 
-static const char *log_file_path = NULL;
-int log_file_size;
+char *log_file_path;
+char *stats_file_path="stats.log";
+int fs_size;
+int fs_read=0;
+int fs_write=0;
+int gc_called=0;
+int gc_write=0;
+time_t mount;
+time_t unmount;
 
 static int lfs_getattr(const char *path, struct stat *stbuf){
 
   int res;
-
   res = lstat(path, stbuf);
   if (res == -1)
     return -errno;
@@ -70,9 +76,16 @@ static int lfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
   return 0;
 }
 
+static int lfs_chmod(const char *path, mode_t mode){
+  int res;
+  res = chmod(path, mode);
+  if (res == -1)
+    return -errno;
+  return 0;
+}
+
 static int lfs_open(const char *path, struct fuse_file_info *fi){
   int res;
-
   res = open(path, fi->flags);
   if (res == -1)
     return -errno;
@@ -117,6 +130,7 @@ static int lfs_write(const char *path, const char *buf, size_t size,
 
 static int lfs_mkdir(const char *path, mode_t mode){
   int res;
+  //printf("this is a test call in mkdir");
   res = mkdir(path, mode);
   if (res == -1)
     return -errno;
@@ -153,6 +167,7 @@ static struct fuse_operations lfs_oper = {
   .statfs         = lfs_statfs,
   .readlink       = lfs_readlink,
   .access         = lfs_access,
+  .chmod          = lfs_chmod,
   // file operations
   .open           = lfs_open,
   .read           = lfs_read,
@@ -163,6 +178,16 @@ static struct fuse_operations lfs_oper = {
   .mkdir          = lfs_mkdir,
 };
 
+void write_stats(){
+  FILE *fp;
+  fp=fopen(stats_file_path,O_WRONLY);
+  fseek(fp,0l,SEEK_END);
+  //fwrite(
+
+
+}
+
+
 int main(int argc, char *argv[]){
 
   // Initialize an empty argument list
@@ -170,9 +195,10 @@ int main(int argc, char *argv[]){
   // add program and mountpoint
   fuse_opt_add_arg(&mountpt, argv[0]);
   fuse_opt_add_arg(&mountpt, argv[1]);
-  //fuse_opt_add_arg(&mountpt, argv[4]);// for permission
-  log_file_path= argv[2];
-  log_file_size= argv[3];
-     
+  fuse_opt_add_arg(&mountpt, argv[2]);// for debug
+  fuse_opt_add_arg(&mountpt, argv[3]);// for debug
+  log_file_path= argv[3];
+  fs_size= argv[4];
+  umask(0);
   return fuse_main(mountpt.argc, mountpt.argv, &lfs_oper, NULL);
 }
