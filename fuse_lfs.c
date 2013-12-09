a7f4e0ad8da74cead4631dbdee45e87ec8e3c7a2
Theophilus Mensah <theomens@man.cs.rutgers.edu>
2013-12-09 02:04:48 -0500

    stats update

--- fuse_lfs.c
+++ fuse_lfs.c
@@ -1,16 +1,18 @@
 #define FUSE_USE_VERSION 30
-
-#include "stdlib.h"
-#include <fuse.h>
+#include <fuse_lowlevel.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <assert.h>
 #include <sys/time.h>
+#include <time.h>
 
+FILE *log_fp;
 char *log_file_path;
-char *stats_file_path="stats.log";
+char *stats_file_path="stats.txt";
 int fs_size;
 int fs_read=0;
 int fs_write=0;
@@ -18,178 +20,230 @@ int gc_called=0;
 int gc_write=0;
 time_t mount;
 time_t unmount;
+int block_size = 4096;
+int num_blocks;
+int free_space;
+int offset =0;
 
-static int lfs_getattr(const char *path, struct stat *stbuf){
+typedef struct meta_data{
+  char *file_name;
+  char *file_type;
+  char *parent;
+  char **children;
+  char *created;
+  char *updated;
+  int data_size;
+  int meta_size;
+  int data_offset;
+}Meta_data;
 
-  int res;
-  res = lstat(path, stbuf);
-  if (res == -1)
-    return -errno;
+Meta_data **dictionary;
+int curr_block=3;
+static const char *hello_str = "Hello World!\n";
+static const char *hello_name = "hello";
 
+static int lfs_stat(fuse_ino_t ino, struct stat *stbuf){
+
+  stbuf->st_ino = ino;
+  switch (ino) {
+  case 1:
+    stbuf->st_mode = S_IFDIR | 0755;
+    stbuf->st_nlink = 2;
+    break;
+  case 2:
+    stbuf->st_mode = S_IFREG | 0444;
+    stbuf->st_nlink = 1;
+    stbuf->st_size = strlen(hello_str);
+    break;
+  default:
+    return -1;
+  }
   return 0;
 }
 
-static int lfs_access(const char *path, int mask){
-  int res;
-
-  res = access(path, mask);
-  if (res == -1)
-    return -errno;
-
-  return 0;
-}
-
-static int lfs_readlink(const char *path, char *buf, size_t size){
-  int res;
-
-  res = readlink(path, buf, size - 1);
-  if (res == -1)
-    return -errno;
-
-  buf[res] = '\0';
-  return 0;
-}
-
-static int lfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-                         off_t offset, struct fuse_file_info *fi){
-  //The DIR stuff was causing it to copy all the ilab directories.  This doesn't
-  //However I still can't figure out how to make a file appear in the new directory -.-
-  (void) offset;
-  (void)  fi;
-  
-  if(strcmp(path, "/") != 0)
-    return -ENOENT;
-    
-  filler(buf, ".", NULL, 0);
-  filler(buf, "..", NULL, 0);
-  filler(buf, log_file_path + 1, NULL, 0);
-  
-  return 0;
-}
-
-static int lfs_chmod(const char *path, mode_t mode){
-  int res;
-  res = chmod(path, mode);
-  if (res == -1)
-    return -errno;
-  return 0;
-}
-
-static int lfs_open(const char *path, struct fuse_file_info *fi){
-  int res;
-  res = open(path, fi->flags);
-  if (res == -1)
-    return -errno;
-
-  close(res);
-  return 0;
-
-}
-
-static int lfs_read(const char *path, char *buf, size_t size, off_t offset,
-                      struct fuse_file_info *fi){
-  int fd;
-  int res;
-
+static void lfs_ll_getattr(fuse_req_t req, fuse_ino_t ino,
+                             struct fuse_file_info *fi){
+  struct stat stbuf;
   (void) fi;
-  fd = open(path, O_RDONLY);
-  if (fd == -1)
-    return -errno;
-
-  res = pread(fd, buf, size, offset);
-  if (res == -1)
-    res = -errno;
-
-  close(fd);
-  return res;
+  memset(&stbuf, 0, sizeof(stbuf));
+  if (lfs_stat(ino, &stbuf) == -1)
+    fuse_reply_err(req, ENOENT);
+  else
+    fuse_reply_attr(req, &stbuf, 1.0);
 }
 
-static int lfs_write(const char *path, const char *buf, size_t size,
-                     off_t offset, struct fuse_file_info *fi){
-  int fd;
-  int res;
-  (void) fi;
-  fd = open(path, O_WRONLY);
-  if (fd == -1)
-    return -errno;
-  res = pwrite(fd, buf, size, offset);
-  if (res == -1)
-    res = -errno;
-  close(fd);
-  return res;
+static void lfs_ll_lookup(fuse_req_t req, fuse_ino_t parent, const char *name){
+  struct fuse_entry_param e;
+  if (parent != 1 || strcmp(name, hello_name) != 0)
+    fuse_reply_err(req, ENOENT);
+  else {
+    memset(&e, 0, sizeof(e));
+    e.ino = 2;
+    e.attr_timeout = 1.0;
+    e.entry_timeout = 1.0;
+    lfs_stat(e.ino, &e.attr);
+    fuse_reply_entry(req, &e);
+  }
 }
 
-static int lfs_mkdir(const char *path, mode_t mode){
-  int res;
-  //printf("this is a test call in mkdir");
-  res = mkdir(path, mode);
-  if (res == -1)
-    return -errno;
-  return 0;
-}
-
-static int lfs_rmdir(const char *path){
-  int res;
-  res = rmdir(path);
-  if (res == -1)
-    return -errno;
-  return 0;
-}
-
-static int lfs_rename(const char *from, const char *to){
-  int res;
-  res = rename(from, to);
-  if (res == -1)
-    return -errno;
-  return 0;
-}
-
-static int lfs_statfs(const char *path, struct statvfs *stbuf){
-  int res;
-  res = statvfs(path, stbuf);
-  if (res == -1)
-    return -errno;
-  return 0;
-}
-
-static struct fuse_operations lfs_oper = {
-  .getattr        = lfs_getattr,
-  .readdir        = lfs_readdir,
-  .statfs         = lfs_statfs,
-  .readlink       = lfs_readlink,
-  .access         = lfs_access,
-  .chmod          = lfs_chmod,
-  // file operations
-  .open           = lfs_open,
-  .read           = lfs_read,
-  .write          = lfs_write,
-  .rename         = lfs_rename,
-  //dir operations
-  .rmdir          = lfs_rmdir,
-  .mkdir          = lfs_mkdir,
+struct dirbuf {
+  char *p;
+  size_t size;
 };
 
-void write_stats(){
-  FILE *fp;
-  fp=fopen(stats_file_path,O_WRONLY);
-  fseek(fp,0l,SEEK_END);
-  //fwrite(
+static void dirbuf_add(fuse_req_t req, struct dirbuf *b, const char *name,
+                       fuse_ino_t ino){
+  struct stat stbuf;
+  size_t oldsize = b->size;
+  b->size += fuse_add_direntry(req, NULL, 0, name, NULL, 0);
+  b->p = (char *) realloc(b->p, b->size);
+  memset(&stbuf, 0, sizeof(stbuf));
+  stbuf.st_ino = ino;
+  fuse_add_direntry(req, b->p + oldsize, b->size - oldsize, name, &stbuf,
+		    b->size);
+}
+#define min(x, y) ((x) < (y) ? (x) : (y))
+static int reply_buf_limited(fuse_req_t req, const char *buf, size_t bufsize,
+                             off_t off, size_t maxsize){
+  if (off < bufsize)
+    return fuse_reply_buf(req, buf + off,
+			  min(bufsize - off, maxsize));
+  else
+    return fuse_reply_buf(req, NULL, 0);
+}
+
+static void lfs_ll_readdir(fuse_req_t req, fuse_ino_t ino, size_t size,
+                             off_t off, struct fuse_file_info *fi){
+  (void) fi;
+  if (ino != 1)
+    fuse_reply_err(req, ENOTDIR);
+  else {
+    struct dirbuf b;
+    memset(&b, 0, sizeof(b));
+    dirbuf_add(req, &b, ".", 1);
+    dirbuf_add(req, &b, "..", 1);
+    dirbuf_add(req, &b, hello_name, 2);
+    reply_buf_limited(req, b.p, b.size, off, size);
+    free(b.p);
+  }
+}
+
+static void lfs_ll_open(fuse_req_t req, fuse_ino_t ino,
+                          struct fuse_file_info *fi){
+  if (ino != 2)
+    fuse_reply_err(req, EISDIR);
+  else if ((fi->flags & 3) != O_RDONLY)
+    fuse_reply_err(req, EACCES);
+  else
+    fuse_reply_open(req, fi);
+}
+
+static void lfs_ll_read(fuse_req_t req, fuse_ino_t ino, size_t size,
+                          off_t off, struct fuse_file_info *fi){
+  (void) fi;
+  assert(ino == 2);
+  reply_buf_limited(req, hello_str, strlen(hello_str), off, size);
+}
+
+static void lfs_ll_write(fuse_req_t req, fuse_ino_t ino, const char *buf, 
+			 size_t size, off_t off, struct fuse_file_info *fi){
+  Meta_data new_blk;
+  new_blk.file_name=fi->fh;
+  printf("%s",fi->fh);
+  dictionary[curr_block+1]= malloc(sizeof(Meta_data)); 
+
+  if(ino != 1)
+    fuse_reply_err(req,ENOENT);
+  else
+    //pwrite(log_fp, buf, size, offset);
+    fuse_reply_write(req,fi);
 
 
+}
+
+static struct fuse_lowlevel_ops lfs_ll_oper = {
+  .lookup         = lfs_ll_lookup,
+  .getattr        = lfs_ll_getattr,
+  .readdir        = lfs_ll_readdir,
+  .open           = lfs_ll_open,
+  .read           = lfs_ll_read,
+  .write          = lfs_ll_write,
+};
+
+int write_stats(){
+  FILE *stat_fp;
+  int n;
+  char buff[300];
+  struct tm *tsm,*tsu;
+  char buf1[80];
+  char buf2[80];
+  
+  tsm = localtime(&mount);
+  strftime(buf1, sizeof(buf1), "%a %Y-%m-%d %H:%M:%S %Z", tsm);
+  tsu = localtime(&unmount);
+  strftime(buf2, sizeof(buf2), "%a %Y-%m-%d %H:%M:%S %Z", tsu);
+  n=sprintf(buff,"Mount time:%s\nfs_size: %dMB\nfs_read:%dMB\nfs_write:%dMB\ngc_called:%d\ngc_write:%dMB\nUnmount\
+ time:%s\n",buf1,(fs_size/1000000),fs_read,fs_write,
+            gc_called,gc_write,buf2);
+
+  stat_fp = fopen (stats_file_path,"a+");
+  if (stat_fp == NULL) {
+    printf ("error opening stats\n");
+    return 1;
+  }
+  fseek(stat_fp,0l,SEEK_END);
+  fwrite(buff,sizeof(char),n,stat_fp);
+  fclose(stat_fp);
+  return 0;
 }
 
 
 int main(int argc, char *argv[]){
-
-  // Initialize an empty argument list
+  // Initialize an empty argument list                                                  
   struct fuse_args mountpt = FUSE_ARGS_INIT(0, NULL);
-  // add program and mountpoint
+  // add program and mountpoint                                   
   fuse_opt_add_arg(&mountpt, argv[0]);
   fuse_opt_add_arg(&mountpt, argv[1]);
-  fuse_opt_add_arg(&mountpt, argv[2]);// for debug
-  fuse_opt_add_arg(&mountpt, argv[3]);// for debug
-  log_file_path= argv[3];
-  fs_size= argv[4];
-  umask(0);
-  return fuse_main(mountpt.argc, mountpt.argv, &lfs_oper, NULL);
+  fuse_opt_add_arg(&mountpt, argv[2]);// for debug                            
+  fuse_opt_add_arg(&mountpt, argv[3]);// for debug                   
+
+  log_file_path= argv[4];
+  fs_size=atoi(argv[5])*1000000;
+  num_blocks=fs_size/block_size;
+  free_space=block_size*4;
+  dictionary= malloc(num_blocks*sizeof(int));
+  log_fp = fopen (log_file_path,"a+");
+  if (log_fp == NULL) {
+    printf ("Data file could not be opened\n");
+    return 1;
+  }
+  mount=time(NULL);
+
+  struct fuse_args args = FUSE_ARGS_INIT(mountpt.argc, mountpt.argv);
+  struct fuse_chan *ch;
+  char *mountpoint;
+  int err = -1;
+  if (fuse_parse_cmdline(&args, &mountpoint, NULL, NULL) != -1 &&
+      (ch = fuse_mount(mountpoint, &args)) != NULL) {
+    struct fuse_session *se;
+    se = fuse_lowlevel_new(&args, &lfs_ll_oper,
+			   sizeof(lfs_ll_oper), NULL);
+    if (se != NULL) {
+      if (fuse_set_signal_handlers(se) != -1) {
+	fuse_session_add_chan(se, ch);
+	/* Block until ctrl+c or fusermount -u */
+	err = fuse_session_loop(se);
+	fuse_remove_signal_handlers(se);
+	fuse_session_remove_chan(ch);
+      }
+      fuse_session_destroy(se);
+      unmount=time(NULL);
+      write_stats();
+    }
+    fuse_unmount(mountpoint, ch);
+  }
+  fuse_opt_free_args(&args);
+  free(dictionary);
+  fclose(log_fp);
+  return err ? 1 : 0;
 }
--- master.c
+++ master.c
@@ -8,15 +8,20 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/time.h>
-#include <dirent.h>
 
-static const char *log_file_path = NULL;
-int log_file_size;
+char *log_file_path;
+char *stats_file_path="stats.log";
+int fs_size;
+int fs_read=0;
+int fs_write=0;
+int gc_called=0;
+int gc_write=0;
+time_t mount;
+time_t unmount;
 
 static int lfs_getattr(const char *path, struct stat *stbuf){
 
   int res;
-
   res = lstat(path, stbuf);
   if (res == -1)
     return -errno;
@@ -47,32 +52,31 @@ static int lfs_readlink(const char *path, char *buf, size_t size){
 
 static int lfs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
                          off_t offset, struct fuse_file_info *fi){
-  DIR *dp;
-  struct dirent *de;
-
+  //The DIR stuff was causing it to copy all the ilab directories.  This doesn't
+  //However I still can't figure out how to make a file appear in the new directory -.-
   (void) offset;
-  (void) fi;
+  (void)  fi;
+  
+  if(strcmp(path, "/") != 0)
+    return -ENOENT;
+    
+  filler(buf, ".", NULL, 0);
+  filler(buf, "..", NULL, 0);
+  filler(buf, log_file_path + 1, NULL, 0);
+  
+  return 0;
+}
 
-  dp = opendir(path);
-  if (dp == NULL)
+static int lfs_chmod(const char *path, mode_t mode){
+  int res;
+  res = chmod(path, mode);
+  if (res == -1)
     return -errno;
-
-  while ((de = readdir(dp)) != NULL) {
-    struct stat st;
-    memset(&st, 0, sizeof(st));
-    st.st_ino = de->d_ino;
-    st.st_mode = de->d_type << 12;
-    if (filler(buf, de->d_name, &st, 0))
-      break;
-  }
-
-  closedir(dp);
   return 0;
 }
 
 static int lfs_open(const char *path, struct fuse_file_info *fi){
   int res;
-
   res = open(path, fi->flags);
   if (res == -1)
     return -errno;
@@ -117,6 +121,7 @@ static int lfs_write(const char *path, const char *buf, size_t size,
 
 static int lfs_mkdir(const char *path, mode_t mode){
   int res;
+  //printf("this is a test call in mkdir");
   res = mkdir(path, mode);
   if (res == -1)
     return -errno;
@@ -153,6 +158,7 @@ static struct fuse_operations lfs_oper = {
   .statfs         = lfs_statfs,
   .readlink       = lfs_readlink,
   .access         = lfs_access,
+  .chmod          = lfs_chmod,
   // file operations
   .open           = lfs_open,
   .read           = lfs_read,
@@ -163,6 +169,16 @@ static struct fuse_operations lfs_oper = {
   .mkdir          = lfs_mkdir,
 };
 
+void write_stats(){
+  FILE *fp;
+  fp=fopen(stats_file_path,O_WRONLY);
+  fseek(fp,0l,SEEK_END);
+  //fwrite(
+
+
+}
+
+
 int main(int argc, char *argv[]){
 
   // Initialize an empty argument list
@@ -170,9 +186,10 @@ int main(int argc, char *argv[]){
   // add program and mountpoint
   fuse_opt_add_arg(&mountpt, argv[0]);
   fuse_opt_add_arg(&mountpt, argv[1]);
-  fuse_opt_add_arg(&mountpt, argv[4]);// for permission
-  log_file_path= argv[2];
-  log_file_size= argv[3];
-     
+  fuse_opt_add_arg(&mountpt, argv[2]);// for debug
+  fuse_opt_add_arg(&mountpt, argv[3]);// for debug
+  log_file_path= argv[3];
+  fs_size= argv[4];
+  umask(0);
   return fuse_main(mountpt.argc, mountpt.argv, &lfs_oper, NULL);
 }
